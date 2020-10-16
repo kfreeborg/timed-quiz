a4143feec0dbd71c1b0fff7a9fc6f0354daf17df
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-15 23:30:51 -0700

    start button function started

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var startButtonEl = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("question-container");
+
+
+startButtonEl.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  startButtonEl.classList.add("hide");
+  questionContainerEl.classList.remove("hide");
+};
+
+function timer() {
+
+};
+
+function nextQuestion() {
+
+};
+
+function answer() {
+
+};
+
+function highScore() {
+
+};
