d1572ea087d80c4d616f445f3894f881e05b2aa7
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-16 17:17:07 -0700

    showing answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 var startButtonEl = document.getElementById("start-btn");
 var questionContainerEl = document.getElementById("question-container");
-
-/*var questionEl = document.getElementById("question");
-var answerEl = document.querySelector("#answer-buttons"); */
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answer-buttons");
 var choiceEl = Array.from(document.getElementsByClassName("choice"));
 startButtonEl.addEventListener("click", startQuiz);
 
@@ -32,9 +31,27 @@ function nextQuestion() {
   questionCounter++;
   var questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
-  questionContainerEl.innerText = currentQuestion.ques;
+  questionEl.innerText = currentQuestion.ques;
 
-  choiceEl.innerText = currentQuestion["option"];
+  /* choiceEl.forEach((ans) => {
+    ans.innerText = option.text;
+    ans.classList.add("choice");
+  }); */
+
+  currentQuestion.options.forEach((option) => {
+
+    // answerEl.innerText = option;
+    var button = document.createElement("button");
+    //button.innerText = option;
+    button.textContent = option;
+    button.classList.add("btn");
+    answerEl.appendChild(button);
+    console.log(button);
+    // console.log(currentQuestion[option]);
+
+  });
+
+
 };
 
 // highscore
@@ -42,42 +59,27 @@ function nextQuestion() {
 var questions = [
   {
     ques: "What is JavaScript?1",
-    option: "answer 1",
-    option: "answer 2",
-    option: "answer 3",
-    option: "answer 4",
-    answer: "answer 2"
+    options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    answer: "answer 1"
   },
   {
     ques: "What is JavaScript?2",
-    option: "answer 1",
-    option: "answer 2",
-    option: "answer 3",
-    option: "answer 4",
-    answer: "answer 2"
+    options: ["answer 5", "answer 6", "answer 7", "answer 8"],
+    answer: "answer 1"
   },
   {
     ques: "What is JavaScript?3",
-    option: "answer 1",
-    option: "answer 2",
-    option: "answer 3",
-    option: "answer 4",
-    answer: "answer 2"
+    options: ["answer 9", "answer 10", "answer 11", "answer 12"],
+    answer: "answer 1"
   },
   {
     ques: "What is JavaScript?4",
-    option: "answer 1",
-    option: "answer 2",
-    option: "answer 3",
-    option: "answer 4",
-    answer: "answer 2"
+    options: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 1"
   },
   {
     ques: "What is JavaScript?5",
-    option: "answer 1",
-    option: "answer 2",
-    option: "answer 3",
-    option: "answer 4",
-    answer: "answer 2"
+    options: ["answer 17", "answer 18", "answer 19", "answer 20"],
+    answer: "answer 1"
   }
 ];
